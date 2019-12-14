e1fa257db7fe13e64660bb4e974681ecdda1f028
Joseph Ledbetter <josephb.ledbetter@gmail.com>
2019-12-13 21:51:52 -0500

    finalized js for the bamazon inventory. selection synced and prompts completed. Updating the list for the customer view.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,22 +22,53 @@ function queryBamazonProducts() {
             console.log(`${res[i].id} | ${res[i].product} | ${res[i].product_price} | ${res[i].stock_quantity}`)
         }
         console.log("....................");
+
+        console.log('Item inquiry');
+
+        purchasePrompt();
         
-        console.log('before inquirer');
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'purchaseAmount',
-            message: 'PLEASE SELECT THE ID OF THE ITEM YOU WOULD LIKE TO PURCHASE'
-        }
-    ])
-    .then(function(answers) {
-        if (answers.amount);
-        else { }
-    });
-    })
-};
+        function purchasePrompt(){
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'purchaseItem',
+                    message: 'PLEASE SELECT THE ID OF THE ITEM YOU WOULD LIKE TO PURCHASE',
+                },
+                {
+                    type: 'input',
+                    name: 'purchaseAmount',
+                    message: 'PLEASE SELECT THE QUANTITY YOU WOULD LIKE TO PURCHASE.',
+                }
+            ])
+            .then(function (answers) {
 
+                let quantitySelected = answers.purchaseAmount;
+                let idRequested = answers.purchaseItem;
+                console.log('You have selected item number ' + idRequested + ' with a quantity of ' + quantitySelected + ' units.')
+                purchaseOrder(idRequested, quantitySelected);
+                });
 
+                function purchaseOrder(purchaseItem, purchaseAmount){
+                    connection.query(`SELECT * FROM products WHERE id = ${purchaseItem}` , function (err, res){
+                        if (err) throw err;
+                        if (purchaseAmount <= res[0].stock_quantity){
+                            let totalCost = res[0].product_price * purchaseAmount;
+                            console.log("................................")
+                            console.log(`Good news we have plent of item ${purchaseItem} in stock!`);
+                            console.log("................................")
+                            console.log(`Your total cost will be ${totalCost}`);
+                            console.log("................................")
+                            console.log("................................")
+                            console.log("Thank you for shopping with Bamazon!")
+                            console.log("................................")
+                            console.log("................................")
+                            console.log("................................")
+                            console.log("...UPDATED INVENTORY...")
+                            connection.query("UPDATE products SET stock_quantity = stock_quantity - " + purchaseAmount + " WHERE id = " + purchaseItem)}
 
+                        else {console.log(`Insufficient quanitity ${res[0].product}  to complete the order.`);
+                    };
+                    queryBamazonProducts();
+                 });
+    }}})}
